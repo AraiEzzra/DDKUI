d4d21dd65fa7bbfa2cacbebd9a320a7cc985c368
hotam-singh <hotam.hbti123@gmail.com>
2018-03-13 17:41:05 +0530

    created server

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var http = require('http');
+var cors = require('cors');
+var path = require('path');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+
+var app = express();
+var port = process.env.PORT || '7000';
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'public'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true, limit: '2mb', parameterLimit: 5000 }));
+app.use(bodyParser.json({ limit: '2mb' }));
+app.use(cookieParser());
+
+app.get('/', function (req, res) {
+    res.render('wallet.html', { layout: false });
+});
+
+app.listen(port, function() {
+    console.log('server running on port : ', port);
+})
