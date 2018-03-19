77494278eff1fe20213654f1fafe2713bd3b596d
navin <navin@navin.purohit@oodlestechnologies.com>
2018-03-19 12:27:30 +0530

    migration changes

--- public/js/controllers/existingETPSUserController.js
+++ public/js/controllers/existingETPSUserController.js
@@ -20,30 +20,22 @@ angular.module('ETPApp').controller('existingETPSUserController', ['$scope', '$r
         });
     }
 
-    $scope.generateApiKey = function (mykey) {
-
-        // DO NOT EDIT THIS FUNCTION
-        // THE DYNAMIC API KEY WILL BE VALID FOR 10 SEC
-        var time_sec = Math.floor(Date.now() / 1000) + 10;
-        var dynamic_key = mykey + "####" + time_sec;
-
-        return btoa(dynamic_key);
-    }
-
     // function to validate existing ETPS user from ETP_test database
     $scope.validateExistingUser = function (username, password) {
+        var post = "username=" + btoa(username) + "&password=" + btoa(password);
 
-        $http.post($rootScope.serverUrl + "/api/accounts/existingETPSUser/validate", {
-            username: btoa(username),
-            password: btoa(password)
+        $http.post("/api/accounts/existingETPSUser/validate", {
+
+            data: post
         })
             .success(function (resp) {
-                if (!resp.records) {
-                    $scope.errorMessage = resp.message;
+
+                if (!resp.success) {
+                    $scope.errorMessage = resp.error;
                 } else {
                     var userInfo = {};
-                    Object.assign(userInfo, resp.records);
-                    $http.post($rootScope.serverUrl + "/api/accounts/existingETPSUser", { userInfo: userInfo }).then(function (response) {
+                    Object.assign(userInfo, resp.userInfo);
+                    $http.post("/api/accounts/existingETPSUser", { userInfo: userInfo }).then(function (response) {
                         if (response.data.success) {
                             if (response.data.isMigrated) {
                                 $scope.errorMessage = 'User is already migrated';
