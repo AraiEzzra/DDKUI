1a9d5d9b9a34a8ea136b610a561f2f6fe4d249c8
monu thakur <monu@monu.thakur>
2018-04-06 15:49:22 +0530

    Add form section

--- public/js/controllers/settingsController.js
+++ public/js/controllers/settingsController.js
@@ -1,6 +1,6 @@
 require('angular');
 
-angular.module('ETPApp').controller('settingsController', ['$scope', '$rootScope', '$http', "userService", "$interval", "multisignatureModal", 'gettextCatalog', function ($rootScope, $scope, $http, userService, $interval, multisignatureModal, gettextCatalog) {
+angular.module('ETPApp').controller('settingsController', ['$scope', '$rootScope', '$http', "userService", "$interval", "multisignatureModal", 'gettextCatalog', '$location', function ($rootScope, $scope, $http, userService, $interval, multisignatureModal, gettextCatalog, $location) {
 
     var setPage = function () {
         $scope.view.page = {title: gettextCatalog.getString('Settings'), previous: null};
@@ -54,4 +54,91 @@ angular.module('ETPApp').controller('settingsController', ['$scope', '$rootScope
         });
     }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    $scope.steps = [
+        'Step 1: Download App',
+        'Step 2: Scan QR Code',
+        'Step 3: Backup Key',
+        'Step 4: Enable Google Authentication'
+      ];
+      $scope.selection = $scope.steps[0];
+    
+      $scope.getCurrentStepIndex = function(){
+        // Get the index of the current step given selection
+        return _.indexOf($scope.steps, $scope.selection);
+      };
+    
+      // Go to a defined step index
+      $scope.goToStep = function(index) {
+        if ( !_.isUndefined($scope.steps[index]) )
+        {
+          $scope.selection = $scope.steps[index];
+        }
+      };
+    
+      $scope.hasNextStep = function(){
+        var stepIndex = $scope.getCurrentStepIndex();
+        var nextStep = stepIndex + 1;
+        // Return true if there is a next step, false if not
+        return !_.isUndefined($scope.steps[nextStep]);
+      };
+    
+      $scope.hasPreviousStep = function(){
+        var stepIndex = $scope.getCurrentStepIndex();
+        var previousStep = stepIndex - 1;
+        // Return true if there is a next step, false if not
+        return !_.isUndefined($scope.steps[previousStep]);
+      };
+    
+      $scope.incrementStep = function() {
+        if ( $scope.hasNextStep() )
+        {
+          var stepIndex = $scope.getCurrentStepIndex();
+          var nextStep = stepIndex + 1;
+          $scope.selection = $scope.steps[nextStep];
+        }
+      };
+    
+      $scope.decrementStep = function() {
+        if ( $scope.hasPreviousStep() )
+        {
+          var stepIndex = $scope.getCurrentStepIndex();
+          var previousStep = stepIndex - 1;
+          $scope.selection = $scope.steps[previousStep];
+        }
+      };
+
+
+
+    
+    
+    
+   
+
 }]);
+
+
+
+
+
+
+
+
+
+
+
