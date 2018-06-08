e6ffab2d5911effacbf7c11fcbf8c4b1cb8fc599
hotam-singh <hotam.hbti123@gmail.com>
2018-06-08 16:30:00 +0530

    modifed active delegates

--- public/js/services/delegateService.js
+++ public/js/services/delegateService.js
@@ -19,7 +19,7 @@ angular.module('ETPApp').service('delegateService', function ($http, $rootScope,
     }
 
     var delegates = {
-        topRate: 1,
+        topRate: 3,
         gettingStandBy: false,
         gettingTop: false,
         gettingVoted: false,
