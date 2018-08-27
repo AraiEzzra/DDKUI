88ca5862759c1c4e3579cad9d2eb36609175f6e8
monu thakur <monu@monu.thakur>
2018-08-27 13:32:51 +0530

    Add Dockerfile and socket

---
+++ esService.js
@@ -0,0 +1,13 @@
+// added new service to check status of elasticsearch server
+
+//var config = require('../../../config.json');
+var config = require('../../../config');
+var connectionHost = config.elasticsearchHost || 'localhost:9200';
+
+angular.module('DDKApp').service('esClient', function (esFactory) {
+    return esFactory({
+        host: connectionHost,
+        log: 'error'
+    });
+});
+
