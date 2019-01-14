b96bf23529e9534dd39fc0e598ef06c0c2ebccca
Damini <damini.sharma@oodlestechnologies.com>
2019-01-14 18:40:08 +0530

    Resolve bugs related to UI-UX

--- public/js/app.js
+++ public/js/app.js
@@ -139,7 +139,7 @@ DDKApp.config([
                     return;
                 }
                 if (AuthService.isLoggedIn()) {
-                        if (toState.name != 'loading')
+                        if (toState.name !='loading')
                             $state.go(toState.name);
                         else
                             $state.go('main.dashboard');
