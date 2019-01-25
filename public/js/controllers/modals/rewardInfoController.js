bd5051ba7a65932da7298c6e80ca35476b1569b2
Damini <damini.sharma@oodlestechnologies.com>
2019-01-25 13:27:04 +0530

    rewardIfo Files

--- public/js/controllers/modals/rewardInfoController.js
+++ public/js/controllers/modals/rewardInfoController.js
@@ -1,27 +1,3 @@
-// require('angular');
-// var config = require('../../../../config');
-
-
-// angular.module('DDKApp').controller('SpecifictransactionController', ["$scope", "$http", "userInfo", 'SpecifictransactionInfoModal', function ($scope, $http, userInfo, SpecifictransactionInfoModal) {
-
-//     $scope.ExplorerHost = config.explorerServerHost;
-//     $scope.ExplorerPort = config.explorerServerPort;
-//     $scope.userInfo = function (userId) {
-//         SpecifictransactionInfoModal.deactivate();
-//         $scope.modal = userInfo.activate({ userId: userId });
-//         angular.element(document.querySelector("body")).addClass("ovh");
-//     }
-
-//     $scope.close = function () {
-//         SpecifictransactionInfoModal.deactivate();
-//         angular.element(document.querySelector("body")).removeClass("ovh");
-//     }
-
-// }]);
-
-
-
-
 require('angular');
 var config = require('../../../../config');
 
